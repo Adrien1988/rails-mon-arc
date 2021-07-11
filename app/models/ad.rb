53457932bcf79f3da96bf4fb78a0d7b30a1d79b9
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2021-07-11 17:01:09 +0200

    mise en place de cloudinary

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,5 +3,5 @@ class Ad < ApplicationRecord
   belongs_to :bow
   belongs_to :arrows
   belongs_to :bow_accessory
-  has_one_attached :photo
+  has_many_attached :photos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_073124) do
+ActiveRecord::Schema.define(version: 2021_07_11_142206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "ads", force: :cascade do |t|
     t.string "title"
@@ -27,16 +55,16 @@ ActiveRecord::Schema.define(version: 2021_07_08_073124) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
-    t.bigint "bow_id"
+    t.bigint "arc_id"
     t.bigint "arrow_id"
-    t.bigint "bow_accessory_id"
-    t.index ["bow_accessory_id"], name: "index_ads_on_bow_accessory_id"
-    t.index ["bow_id"], name: "index_ads_on_bow_id"
+    t.bigint "arc_accessory_id"
+    t.index ["arc_accessory_id"], name: "index_ads_on_arc_accessory_id"
+    t.index ["arc_id"], name: "index_ads_on_arc_id"
     t.index ["arrow_id"], name: "index_ads_on_arrow_id"
     t.index ["user_id"], name: "index_ads_on_user_id"
   end
 
-  create_table "bow_accessories", force: :cascade do |t|
+  create_table "arc_accessories", force: :cascade do |t|
     t.string "accessory_name"
     t.string "mark"
     t.string "matter"
@@ -44,7 +72,7 @@ ActiveRecord::Schema.define(version: 2021_07_08_073124) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "bows", force: :cascade do |t|
+  create_table "arcs", force: :cascade do |t|
     t.string "mark"
     t.integer "power"
     t.integer "waist"
@@ -75,8 +103,10 @@ ActiveRecord::Schema.define(version: 2021_07_08_073124) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "ads", "bow_accessories"
-  add_foreign_key "ads", "bows"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "ads", "arc_accessories"
+  add_foreign_key "ads", "arcs"
   add_foreign_key "ads", "arrows"
   add_foreign_key "ads", "users"
 end
